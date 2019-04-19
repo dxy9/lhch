abebc72ef8f0b0067fc951ca6e9b8dde241bd359
dacheng <439585748@qq.com>
2019-04-19 09:29:58 +0800

    git测试 结论

--- src/main/java/com/lhch/fun/git/package-info.java
+++ src/main/java/com/lhch/fun/git/package-info.java
@@ -1,5 +1,8 @@
 /**
  * @author linhaicheng on 2019/04/19
  * @Description 对git的一些测试
+ * 当自己从readme.md 将文件名改成了大写了READEME.md,在git和码云上出现了多余的前面的小写的readme.md文件
+ *
+ * 在包中的文件可以修改文件名而不会出现这样的问题
  */
 package com.lhch.fun.git;
